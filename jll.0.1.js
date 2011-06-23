3d033649758b3ebfd323348016147c2aba5191a0
Mihai Bojin <mihai.bojin@gmail.com>
2011-06-23 01:11:10 +0100

    Implemented ScriptDeferrer Wrote a couple of tests - source code still messy.

--- jll.0.1.js
+++ jll.0.1.js
@@ -1,4 +1,4 @@
-(function(window, document, console) {
+(function(window, document) {
 	// reference to the "head" tag
 	var head = document.getElementsByTagName('head')[0];
 	
@@ -14,6 +14,8 @@
 		// in case of fail it allows events to be triggered
 		var checks = [];
 		
+		var ok = false;
+		
 		// triggers events on script completion (only once)
 		var triggerEvents = function() {
 			if (true == wasLoaded) {
@@ -23,7 +25,7 @@
 			wasLoaded = true;
 			
 			// go through all the events and fire them (they will race at the same time)
-			for (var i=0; i<events.length; i++) {
+			for (var i = 0; i < events.length; i++) {
 				events[i]();
 			}
 		}
@@ -44,40 +46,15 @@
 			// and fire the next item in the deferring queue
 			if (/loaded|complete/.test(this.readyState)) {
 				// check if next item in queue was not triggered for this element
-				if (true !== wasLoaded) {
+				if (true !== wasLoaded && ok) {
 					triggerEvents();
 				}
 			}
 		}
 		
-		// adds events to be executed on script's onload event
-		this.addEvent = function(func) {
-			events.push(func);
-		}
-
-		// append script element to html head
-		this.render = function() {
-			// determine if there are any checks to run before loading the script
-			if (0 < checks.length) {
-				var result = true;
-				for (var i=0; i<checks.length; i++) {
-					var tCheck = checks[i][0]();
-					result &= tCheck;
-					if (!tCheck) {
-						checks[i][1]();
-					}
-				}
-
-				// stop loading if any check has failed
-				if (true != result) {
-					jLL.log("At least one of the conditions failed: " + script.src);
-					return ;
-				}
-			}
-			
-			// append item to html head tag
-			head.appendChild(script);
-			jLL.log("Queued: " + script.src);
+		// public method to get a script src attribute
+		this.src = function() {
+			return script.src;
 		}
 		
 		// add check and trigger
@@ -96,6 +73,148 @@
 			// add condition on index 0 and trigger on index 1
 			checks.push([condition, trigger]);
 		}
+		
+		// adds events to be executed on script's onload event
+		this.addEvent = function(trigger) {
+			// check if event is function and exit if not
+			if ("function" != typeof trigger) {
+				jLL.log("Parameter must be a function; event was not added");
+				return ;
+			}
+			events.push(trigger);
+		}
+
+		// append script element to html head and return true for success
+		// or false when at least a check failed
+		this.render = function() {
+			// determine if there are any checks to run before loading the script
+			if (0 < checks.length) {
+				var result = true;
+				for (var i = 0; i < checks.length; i+=1) {
+					var tCheck = checks[i][0]();
+					result &= tCheck;
+					if (!tCheck) {
+						checks[i][1]();
+					}
+				}
+
+				// stop loading if any check has failed
+				if (true != result) {
+					jLL.log("At least one of the conditions failed: " + script.src);
+					return false;
+				}
+			}
+			
+			// append item to html head tag
+			head.appendChild(script);
+			ok = true;
+			jLL.log("Queued: " + script.src);
+			return true;
+		}
+	}
+	
+	// define the ScriptDeferrer class constructor
+	var ScriptDeferrer = function() {
+		var queue = [];
+		var running = 0;
+		
+		// push script to queue;
+		// onload event can be specified
+		// check to run before loading script can be defined
+		this.push = function(src, onload, check, checkTrigger) {
+			// push multiple script to queue, from array of [src, onload, check, checkTrigger]
+			if ("object" == typeof src && -1 != src.constructor.toString().indexOf("Array")) {
+				for (i in src) {
+					// if src[i] is not an array, skip row
+					if ("object" !== typeof src[i] || -1 === src[i].constructor.toString().indexOf("Array")) {
+						jLL.log("Row is not an array! Skipped row " + i);
+						continue;
+					}
+					
+					// if first argument is not string containing location of script, skip row
+					if ("string" !== typeof src[i][0]) {
+						jLL.log("First argument must be location of script to load! Skipped row " + i);
+						continue;
+					}
+					
+					// create new ScriptTag element
+			        var tag = jLL.buildScriptTag(src[i][0]);
+			
+					// check if onload event was passed
+					if ("undefined" != typeof src[i][1]) {
+				        tag.addEvent(src[i][1]);
+					}
+					
+					// if check closure was passed, define it and it's trigger
+					if ("undefined" != typeof src[i][2]) {
+				        tag.addCheck(src[i][2], src[i][3]);
+					}
+
+					// push ScriptTag into queue
+					queue.push(tag);
+				}
+				
+			// push one script to queue
+			} else if ("string" === typeof src) {
+		        var tag = jLL.buildScriptTag(src);
+		        tag.addCheck(check, checkTrigger);
+		        tag.addEvent(onload);
+				queue.push(tag);
+			
+			// handle wrong arguments
+			} else {
+				jLL.log("Unknown parameter type passed to ScriptDeferer.push: " + typeof arg + "; script was not added to load queue!");
+				return ;
+			}
+		}
+		
+		// start loading script from queue, one by one
+		var loadFromQueue = function() {
+			var tScript = queue.shift();
+			
+			// check if queue is empty in which case finish the process
+			if ("undefined" === typeof tScript) {
+				running = 0;
+				jLL.log("Queue was loaded!");
+				return;
+			}
+			running = 1;
+			jLL.log("Loading " + tScript.src());
+			
+			// define trigger for loading next script in queue
+			var nextInQueue = function(){
+				loadFromQueue();
+			};
+			
+			// trigger loading the next element in queue on script onload
+			tScript.addEvent(nextInQueue);
+			
+			// render (load) script and tie trigger nextInQueue in case
+			// one of the script's defined checks has failed and script was not loaded
+			if (!tScript.render()) {
+				nextInQueue();
+			}
+		}
+
+		// starts loading the queue and checks that queue is not already beeing loaded
+		this.run = function() {
+			if (1 == running) {
+				jLL.log("Queue is already loading!");
+			} else {
+				jLL.log("Loading queue...");
+				loadFromQueue();
+			}
+		}
+
+		// runs the deferrer queue after the onLoad event
+		this.runOnLoad = function() {
+			if (window.attachEvent) {
+				window.attachEvent('onload', this.run);
+			}
+			else {
+				window.addEventListener('load', this.run, false);
+			}
+		}
 	}
 
 	// create JLL class
@@ -104,10 +223,19 @@
 	// create JLL's prototype
 	JLL.prototype = (function(){
 		var enableDebug = false;
+		var deferrer = new ScriptDeferrer();
 		
 		return {
 			buildScriptTag: function(src) {
 				return new ScriptTag(src);
+			},
+			
+			buildScriptDeferrer: function(src) {
+				return new ScriptDeferrer();
+			},
+			
+			staticDeferrer: function() {
+				return deferrer;
 			},
 			
 			// enables logging to console
@@ -136,4 +264,4 @@
 	})();
 	
 	window.jLL = new JLL();
-})(window, document, console);
+})(window, document);
